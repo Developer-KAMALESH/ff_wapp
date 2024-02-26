bfcf4dda08bfb774cce8ba0612b47b0015d01a87
KAMALESH <selvavinayagam944@gmail.com>
2024-02-26 20:22:39 +0530

    first upload

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:grocery_admin_panel/screens/main_screen.dart';
+import 'package:provider/provider.dart';
+
+import 'consts/theme_data.dart';
+import 'controllers/MenuControllerr.dart';
+import 'inner_screens/add_prod.dart';
+import 'providers/dark_theme_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyBGkTGwq8mKOP_3Bus9zRvuJbvspW5W9zU",
+          appId: "1:138117991042:web:04a3ece3942badfac1029b",
+          messagingSenderId: "138117991042",
+          projectId: "farmerzfresh-36ec9",
+          storageBucket: "farmerzfresh-36ec9.appspot.com"));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.setDarkTheme =
+        await themeChangeProvider.darkThemePreference.getTheme();
+  }
+
+  @override
+  void initState() {
+    getCurrentAppTheme();
+    super.initState();
+  }
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: Scaffold(
+                body: Center(
+                  child: Center(
+                    child: Text('App is being initialized'),
+                  ),
+                ),
+              ),
+            );
+          } else if (snapshot.hasError) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              home: Scaffold(
+                body: Center(
+                  child: Center(
+                    child: Text('An error has been occured ${snapshot.error}'),
+                  ),
+                ),
+              ),
+            );
+          }
+          return MultiProvider(
+            providers: [
+              ChangeNotifierProvider(
+                create: (_) => MenuControllerr(),
+              ),
+              ChangeNotifierProvider(
+                create: (_) {
+                  return themeChangeProvider;
+                },
+              ),
+            ],
+            child: Consumer<DarkThemeProvider>(
+              builder: (context, themeProvider, child) {
+                return MaterialApp(
+                    debugShowCheckedModeBanner: false,
+                    title: 'Grocery',
+                    theme:
+                        Styles.themeData(themeProvider.getDarkTheme, context),
+                    home: const MainScreen(),
+                    routes: {
+                      UploadProductForm.routeName: (context) =>
+                          const UploadProductForm(),
+                    });
+              },
+            ),
+          );
+        });
+  }
+}
